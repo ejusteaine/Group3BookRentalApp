f7af658dac406b4c11e295b3dc16814e0f7a2f1f
ejusteaine <vilycharles72@gmail.com>
2022-04-12 22:17:53 -0400

    Added try catch code to handle input mismatch exception

--- BookRentalAp/src/main/java/TestBook/MainTest.java
+++ BookRentalAp/src/main/java/TestBook/MainTest.java
@@ -15,41 +15,47 @@ public class MainTest {
         BookService bkService = new BookService();
 
       int choice;
-      do{
-          System.out.println("----------Book Rental App-------------");
-          System.out.println(
-                   "1 Add a Book\n"+
-                   "2 View Book By Name\n"+
-                   "3 Remove a Book By BookId\n"+
-                   "4 Update Book's Status\n"+
-                   "5 Add a Customer\n"+
-                   "6 Remove a Customer By Id\n"+
-                   "7 Update Customer's Name\n"+
-                   "8 View existing Customer\n"+
-                   "9 Rental Status\n"
-          );
-          System.out.println();
-          System.out.println("Make a Selection: ");
+        try {
+            do {
+                System.out.println("----------Book Rental App-------------");
+                System.out.println(
+                        "1 Add a Book\n" +
+                                "2 View Book By Name\n" +
+                                "3 Remove a Book By BookId\n" +
+                                "4 Update Book's Status\n" +
+                                "5 Add a Customer\n" +
+                                "6 Remove a Customer By Id\n" +
+                                "7 Update Customer's Name\n" +
+                                "8 View existing Customer\n" +
+                                "9 Rental Status\n"
+                );
+                System.out.println();
+                System.out.println("Make a Selection: ");
 
-          choice = input.nextInt();
-          switch (choice){
-              case 1:
-                 bkService.addBook(sc);
-                  break;
-              case 2:
-                  bkService.viewBook(sc);
-                  break;
-              case 3:
-                  bkService.removeBookById(sc);
-                  break;
-              case 4:
-                  bkService.BookStatus(sc);;
-                  break;
-              case 5:
-                  bkService.BookStatus(sc);
-                  break;
-          }
-      }
-while(choice != 4);
+                choice = input.nextInt();
+                switch (choice) {
+                    case 1:
+                        bkService.addBook(sc);
+                        break;
+                    case 2:
+                        bkService.viewBook(sc);
+                        break;
+                    case 3:
+                        bkService.removeBookById(sc);
+                        break;
+                    case 4:
+                        bkService.BookStatus(sc);
+                        ;
+                        break;
+                    case 5:
+                        bkService.BookStatus(sc);
+                        break;
+                }
+            }
+            while (choice != 4) ;
+        }catch(InputMismatchException ex){
+            System.out.println("Invalid input, You should enter a number from 1 - 5");
+        }
+
     }
 }
